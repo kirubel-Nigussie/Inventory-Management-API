75701755c696222936a59ead1772e63a5208739b
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-03-21 14:06:03 +0300

    I have build the model for the inventory and users app

--- Inventory_Management/inventory/models.py
+++ Inventory_Management/inventory/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class InventoryItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- Inventory_Management/users/models.py
+++ Inventory_Management/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False) 
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
